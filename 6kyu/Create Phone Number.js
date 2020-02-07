0860ebaace98eeb22bddfd8790f3aaa826ed7a75
raselrostock <raselrostock@protonmail.com>
2020-02-07 15:40:00 +0600

    Added Create Phone Number in 6kyu

---
+++ 6kyu/Create Phone Number.js
@@ -0,0 +1,14 @@
+function createPhoneNumber(numbers){
+   return '(' + numbers.slice(0,3).join('') + ') ' + numbers.slice(3,6).join('') + '-' + numbers.slice(6).join('');
+}
+
+function createPhoneNumber(numbers){
+  var format = "(xxx) xxx-xxxx";
+  
+  for(var i = 0; i < numbers.length; i++)
+  {
+    format = format.replace('x', numbers[i]);
+  }
+  
+  return format;
+}
